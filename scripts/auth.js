46f89c19c8625482815824aac11a9512d28b601e
Harry Tran <33506591+harrytrandesign@users.noreply.github.com>
2020-10-31 12:57:10 -0700

    closed modal
    close modal

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,5 +9,17 @@ signupForm.addEventListener('submit', (e) => {
 	const userEmail = signupForm['signup-email'].value;
 	const userPassword = signupForm['signup-password'].value;
 
-	console.log(userEmail + userPassword);
+	console.log(userEmail, userPassword);
+
+	// Sign up user into Firebase Auth
+	auth.createUserWithEmailAndPassword(userEmail, userPassword).then(cred => {
+		console.log(cred.user);
+
+		// Close the modal sign up form
+		const modalSignUp = document.querySelector('#modal-signup');
+		M.Modal.getInstance(modalSignUp).close();
+
+		// Reset the form fields to empty
+		signupForm.reset();
+	})
 })
