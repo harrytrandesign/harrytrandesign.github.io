5be2d42f62e84080c8a5ef86c423651a2f680065
Harry Tran <33506591+harrytrandesign@users.noreply.github.com>
2020-11-02 16:43:56 -0800

    Update functions in Auth/Index
    Added function to hide/show menu links
    Function to pull data from firebase rtd and insert into a list

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,34 @@
+// Get data from Firebase Database
+// Just need to grab the database version of Firebase database realtime db
+
+function gotData(data) {
+	// console.log(data.val());
+	setupGuides(data.val());	
+}
+
+function errorData(err) {
+	console.log('Error');
+	console.log(err);
+}
+
+// Listen for any auth state changes
+auth.onAuthStateChanged(user => {
+	console.log(user);
+	if (user) {
+		console.log('User is logged in ', user);
+		setupUiMenu(user);
+
+		var guideRef = firebaseDb.ref('swipe_cloth_shopping/clothes/ladies_dresses');
+		guideRef.on('value', gotData, errorData);
+
+	} else {
+		console.log('User has logged out');
+
+		setupUiMenu();
+		setupGuides([]);
+	}
+})
+
 // Sign up or sign in the user
 const signupForm = document.querySelector('#signup-form');
 // Sign in to Firebase Auth
@@ -30,7 +61,64 @@ logout.addEventListener('click', (e) => {
 	// Prevent default action
 	e.preventDefault();
 
-	auth.signOut().then(() => {
-		console.log("user signed out");
-	})
+	auth.signOut();
+});
+
+// Log In
+const logInForm = document.querySelector('#login-form');
+logInForm.addEventListener('submit', (e) => {
+	// stop just closing the modal
+	e.preventDefault();
+
+	// grab user email + password
+	const userEmail = logInForm['login-email'].value;
+	const userPassword = logInForm['login-password'].value;
+
+	auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred => {
+
+		// Close the modal log in form
+		const modalLogIn = document.querySelector('#modal-login');
+		M.Modal.getInstance(modalLogIn).close();
+
+		// Reset the form fields to empty
+		logInForm.reset();
+
+	});
+});
+
+function closeSubmitForm() {
+	// Close the modal log in form
+	const submitForm = document.querySelector('#modal-create');
+	M.Modal.getInstance(submitForm).close();
+
+	// Reset the form fields to empty
+	submitDataForm.reset()
+}
+
+function writeUserData(title, content) {
+  firebaseDb.ref('testing/').push({
+    label: title,
+    description: content,
+  }, function(error) {
+    if (error) {
+      // The write failed...
+    } else {
+      // Data saved successfully!
+      console.log('save went well');
+      closeSubmitForm();
+    }
+
+  });
+}
+
+const submitDataForm = document.querySelector('#create-form');
+submitDataForm.addEventListener('submit', (e) => {
+
+	e.preventDefault();
+
+	const title = submitDataForm['title'].value;
+	const content = submitDataForm['content'].value;
+
+	writeUserData(title, content);
+
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,63 @@
+// Add to the guides list
+const guideList = document.querySelector('.guides');
+const loggedOutLink = document.querySelectorAll('.logged-out');
+const loggedInLink = document.querySelectorAll('.logged-in');
+
+// Setup Menu Links if logged in or out
+const setupUiMenu = (user) => {
+
+	if (user) {
+
+		// Toggle logged in links
+		loggedInLink.forEach(item => item.style.display = 'block');
+		loggedOutLink.forEach(item => item.style.display = 'none');
+
+	} else {
+
+		// Toggle logged out links
+		loggedInLink.forEach(item => item.style.display = 'none');
+		loggedOutLink.forEach(item => item.style.display = 'block');
+
+	}
+
+}
+
+// Setup the guides
+const setupGuides = (data) => {
+
+	// if (data.length > 0) {
+		let html = '';
+
+		const key = Object.keys(data);
+		console.log(key);
+
+		for (var i = 0; i < key.length; i++) {
+
+			var k = key[i];
+			var header = data[k].userUid;
+			var desc = data[k].imageSourceUrl;
+
+			console.log(header, desc);
+
+		 	const li = `
+
+				<li>
+					<div class = "collapsible-header" grey lighten-4">${header}</div>
+					<div class = "collapsible-body white">${desc}</div>
+				</li>
+
+			`;
+
+			html += li;
+
+		}
+		guideList.innerHTML = html;
+	// } else {
+		// guideList.innerHTML = '<h5 class="center-align">Login to submit looks</h5>'
+	// }
+
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
